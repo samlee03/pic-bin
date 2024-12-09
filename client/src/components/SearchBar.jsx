ab88fa52546a535d6dd1a024fb83391a54bcbf1f
Shenghao <shenghaodong18@gmail.com>
2024-12-08 22:46:30 -0500

    added search bar visually, adding functionality next

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/SearchBar.css'
+
+const SearchBar = () => {
+  return (
+    <div className = "input-wrapper">
+      <input placeholder = "Search Image Here..."></input>
+    </div>
+  )
+};
+
+export default SearchBar
