e41942b7fc760b8b56f0282e46ab8588abf3ff72
Shenghao <shenghaodong18@gmail.com>
2024-12-10 23:16:31 -0500

    updated rest of files to reflect search bar changes

---
+++ client/src/components/SideNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/SideNav.css'
+const SideNav = () => {
+    return (
+      <div className='SideNav'>Header</div>
+    )
+  }
+  
+  export default SideNav
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 import { useEffect, useState } from 'react';
 import SearchBar from '../components/SearchBar';
 import ImagesContainer from '../components/ImagesContainer';
+import "../styles/Home.css"
 const Home = () => {
 
   const [data, setData] = useState([]);
   return (
     <>
       <SearchBar setData = {setData}/>
-      {/* <div>
-        <p>Default: colorful. Press to Fetch nature</p> <button onClick={() => setSearchTerm('nature')}>Fetch</button>
-        <p>Default: colorful. Press to Fetch colorful</p> <button onClick={() => setSearchTerm('colorful')}>Fetch</button>
-      </div> */}
       {data.length == 0
       ? 
-        <p>Loading..</p> // Maybe add some type of loading content here.
+        <p className = "loading">Loading...</p> // Maybe add some type of loading content here.
       :
         <ImagesContainer data={data}/>
       }
