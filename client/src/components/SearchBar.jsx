a881dfd943225e15d82f2029a1017408777b9f60
Shenghao <shenghaodong18@gmail.com>
2024-12-10 22:11:58 -0500

    Search Bar Functionality Added

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import '../styles/SearchBar.css'
+import { useEffect, useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({setData}) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [tempSearchTerm, setTempSearchTerm] = useState('');
+  useEffect(() => {
+    console.log(searchTerm);
+    console.log("Fetching");
+    fetch(`http://localhost:3000/${searchTerm}`)  // Assuming your Node.js API endpoint is /api/data
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => setData(data))
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, [searchTerm, setData]);
+
   return (
     <div className = "input-wrapper">
-      <input placeholder = "Search Image Here..."></input>
+    <input placeholder = "Search Image Here..." 
+      value = {tempSearchTerm} 
+      onChange = {(e) => setTempSearchTerm(e.target.value)}
+      onKeyDown = {(e) => {
+          if(e.key === "Enter"){
+            setSearchTerm(tempSearchTerm);
+          }
+        }
+      }
+    />
     </div>
   )
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,30 +5,13 @@ import ImagesContainer from '../components/ImagesContainer';
 const Home = () => {
 
   const [data, setData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('colorful');
-  useEffect(() => {
-    // Make the fetch request to your backend (you don't need to include localhost:3000)
-    setData([]);
-    console.log("Fetching");
-    fetch(`http://localhost:3000/${searchTerm}`)  // Assuming your Node.js API endpoint is /api/data
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => setData(data))
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, [searchTerm]);
-
   return (
     <>
-      <SearchBar />
-      <div>
+      <SearchBar setData = {setData}/>
+      {/* <div>
         <p>Default: colorful. Press to Fetch nature</p> <button onClick={() => setSearchTerm('nature')}>Fetch</button>
-      </div>
+        <p>Default: colorful. Press to Fetch colorful</p> <button onClick={() => setSearchTerm('colorful')}>Fetch</button>
+      </div> */}
       {data.length == 0
       ? 
         <p>Loading..</p> // Maybe add some type of loading content here.
